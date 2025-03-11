3f698b05331ebccc38ad4192655b95fc51fccbf7
Marco Mazzei <marco.mazzei@maua.br>
2025-03-11 10:44:01 -0300

    Soma de números

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+public class SomaDoisNumeros {
+    public static void main(String[] args) {
+        double primeiroValor;
+        double segundoValor;
+        double resultado;
+        String textoUsuario = JOptionPane.showInputDialog("Digite o primeiro valor:");
+        primeiroValor = Double.parseDouble(textoUsuario);        
+        segundoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor:"));
+        resultado = primeiroValor + segundoValor;
+        String operacao = primeiroValor + " + " + segundoValor + " = " + resultado;
+        JOptionPane.showMessageDialog(null, "O resultado é: " + resultado + "\n" + operacao);
+
+
+
+    }
+}
